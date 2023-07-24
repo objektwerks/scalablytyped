8b9934407ecf990c784dce8a6fcb70e0fe54f570
objektwerks <objektwerks@runbox.com>
2023-07-24 12:01:09 -0400

    enhanced app build def

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -3,6 +3,7 @@ package objektwerks
 import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.HTMLDocument
+import typings.std.stdStrings.canvas
 
 object App:
   def main(args: Array[String]): Unit =
@@ -15,4 +16,8 @@ object App:
     paragraph.id = "pid"
     paragraph.textContent = "Scalajs-ScalablyTyped-Chartjs web app!"
     document.body.appendChild(paragraph)
+
+    val canvas = document.createElement("canvas")
+    canvas.id = "chart"
+    document.body.appendChild(canvas)
     ()
