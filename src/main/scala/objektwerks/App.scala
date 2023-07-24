785c06e37e6bc311a9984ee0cc3d61017c25808c
objektwerks <objektwerks@runbox.com>
2023-07-24 13:07:04 -0400

    edited import

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -2,14 +2,14 @@ package objektwerks
 
 import org.scalajs.dom
 import org.scalajs.dom.document
-import org.scalajs.dom.{Element, HTMLDocument}
+import org.scalajs.dom.{Element, HTMLCanvasElement, HTMLDocument}
 
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters.*
 
 import typings.chartJs.mod.*
 import typings.chartJs.anon.ChartOptions
-import typings.chartJs.distTypesIndexMod.{ChartConfiguration, ChartData, ChartType}
+import typings.chartJs.distTypesIndexMod.{ChartConfiguration, ChartData, ChartDataset, ChartType}
 
 object App:
   def main(args: Array[String]): Unit =
@@ -23,14 +23,14 @@ object App:
     paragraph.textContent = "Scalajs-ScalablyTyped-Chartjs web app!"
     document.body.appendChild(paragraph)
 
-    val canvas = document.createElement("canvas")
+    val canvas = document.createElement("canvas").asInstanceOf[HTMLCanvasElement]
     canvas.id = "chart"
     document.body.appendChild(canvas)
     ()
 
   def buildChart(canvas: Element): Element =
     val chartDataSets = js.Array(
-      new ChartDataSets {
+      new ChartDataSet {
         label = "Data"
         borderWidth = 1
         backgroundColor = "navy"
