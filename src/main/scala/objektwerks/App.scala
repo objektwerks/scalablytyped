b2d561e11d69111bbdeca2c4a0dfa4088ea58011
objektwerks <objektwerks@runbox.com>
2023-07-24 07:38:26 -0400

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -9,19 +9,10 @@ object App:
     document.addEventListener("DOMContentLoaded", { (_: dom.Event) =>
       build(document)
     })
-    scribe.info("*** Scalajs console app!")
 
   def build(document: HTMLDocument): Unit =
     val paragraph = document.createElement("p")
     paragraph.id = "pid"
-    paragraph.textContent = "Scalajs web app!"
+    paragraph.textContent = "Scalajs-ScalablyTyped-Chartjs web app!"
     document.body.appendChild(paragraph)
-
-    val button = document.createElement("button")
-    button.id = "bid"
-    button.textContent = "Print To Console"
-    button.addEventListener("click", { (_: dom.MouseEvent) =>
-      scribe.info("*** Button clicked!")
-    })
-    document.body.appendChild(button)
     ()
