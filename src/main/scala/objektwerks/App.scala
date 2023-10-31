10769367bc3ee5d8bc4fbcc751e1696c14fbe3ae
objektwerks <objektwerks@runbox.com>
2023-10-30 20:20:06 -0400

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,15 +1,13 @@
 package objektwerks
 
 import org.scalajs.dom
+import org.scalajs.dom.*
 import org.scalajs.dom.document
-import org.scalajs.dom.{HTMLCanvasElement, Element, HTMLDocument}
 
-import scala.scalajs.js
-import scala.scalajs.js.JSConverters.*
+// import scala.scalajs.js
+// import scala.scalajs.js.JSConverters.*
 
-import typings.chartJs.mod.*
-import typings.chartJs.anon.ChartType
-import typings.chartJs.distTypesIndexMod.{ChartConfiguration, ChartData, ChartDataset}
+// import typings.chartJs.mod.*
 
 object App:
   def main(args: Array[String]): Unit =
@@ -17,23 +15,4 @@ object App:
       build(document)
     })
 
-  def build(document: HTMLDocument): Unit =
-    val chartType = new ChartType { `type` = "line" }
-    val dataArray = js.Array[Int](1, 2, 3)
-    val chartDatasetProperties = new ChartDataset()
-    chartDatasetProperties.set("type", chartType)
-    chartDatasetProperties.set("data", dataArray)
-
-    val chartDatasets = js.Array[ChartDataset](chartDatasetProperties.asInstanceOf[ChartDataset])
-    
-    val chartData = new ChartData {
-      datasets = chartDatasets
-    }
-
-    val chartConfig = new ChartConfiguration {
-      data = chartData
-    }
-
-    val canvas = document.getElementById("chart").asInstanceOf[HTMLCanvasElement]
-    val chart = Chart(item = canvas, config = chartConfig)
-    ()
+  def build(document: HTMLDocument): Unit = ??? // Rebuild!!!
